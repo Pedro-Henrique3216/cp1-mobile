12f5a8f9ce3c1d3db190c39d97014bcda4858176
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-03-13 21:42:53 -0300

    refactor: adjust styling

--- components/Result.js
+++ components/Result.js
@@ -1,13 +1,28 @@
+import { StyleSheet } from "react-native";
 import { Text, View } from "react-native";
 
 export default function Result({nome, valorOriginal, novoValor, aumento, porcentagem}){
     return (
-        <View>
-            <Text>Nome do produto: {nome}</Text>
-            <Text>Valor Original do produto: {valorOriginal}R$</Text>
-            <Text>Novo valor do Produto: {novoValor}R$</Text>
-            <Text>Quantidade do aumento do produto: {aumento}R$</Text>
-            <Text>Porcentagem que foi aumentada o produto: {porcentagem}%</Text>
+        <View style={Style.model}>
+            <Text style={Style.texto}>Nome do produto: {nome}</Text>
+            <Text style={Style.texto}>Valor Original do produto: {valorOriginal}R$</Text>
+            <Text style={Style.texto}>Novo valor do Produto: {novoValor}R$</Text>
+            <Text style={Style.texto}>Quantidade do aumento do produto: {aumento}R$</Text>
+            <Text style={Style.texto}>Porcentagem que foi aumentada o produto: {porcentagem}%</Text>
         </View>
     )
 }
+
+const Style = StyleSheet.create({
+    model: {
+        backgroundColor: 'green',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10
+    },
+    texto: {
+        color: 'white',
+        fontSize: 20,
+        textAlign: 'center'
+    }
+})
